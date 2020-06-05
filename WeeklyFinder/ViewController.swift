09b661d2f8f2849273d49adf9dd6c52cf7105687
Dmitriy Danilin <danilindn@icloud.com>
2020-06-05 15:42:43 +0300

    финалочка

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -18,7 +18,31 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
-    @IBOutlet weak var findDay: UIButton!
+    @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dataTF.text, let month = monfTF.text, let year = yearTF.text else { return }
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let weekdey = dateFormatter.string(from: date)
+        let capitalizedWeekDay = weekdey.capitalized
+        
+        resultLabel.text = capitalizedWeekDay
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     
 }
 
